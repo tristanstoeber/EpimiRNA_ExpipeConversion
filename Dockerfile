de7966bcb94121a5a946ce6403399c2b01b6cc6d
tristanstoeber <tristan.stoeber@posteo.net>
2022-03-24 16:09:54 +0100

    first working version

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM jupyter/tensorflow-notebook:latest
+
+USER root
+
+RUN apt-get update
+
+USER jovyan
+RUN pip install odfpy
+RUN pip install exdir
+RUN pip install expipe
+RUN pip install pyedflib
+RUN pip install mat73
---
+++ tools.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+
+def test_consistency_of_annotation_table(t):
+    """A collection of tests to make sure that the annotation
+    table has no obvious mistakes
+    
+    Parameters:
+    -----------
+    t : pandas Dataframe
+    
+    Returns:pandas Dataframe
+    -----------
+    void
+    
+    """
+    
+    # -------------------
+    # One aninal, one channel. 
+    # -----
+    # Each animal is associated with one recording channel. 
+    # Thus each animal must only have one channel entry.
+    gpb_ID = t.groupby('ID')
+    for n_i, gp_i in gpb_ID:
+        assert(len(gp_i['Channel'].unique())==1)
+    # Shared channels between animals are not allowed to
+    # overlap on a given day.
+    gpb_ID = t.groupby(['Date', 'Channel', 'Machine'])
+    for n_i, gp_i in gpb_ID:
+        assert(len(gp_i['ID'].unique())==1)
+        
+
+def actions_to_dataframe(actions):
+    ls_actions = []
+    for name_action, action in actions.items():
+        # extract attribues
+        attr = action.attributes
+        dct = {}
+        dct['name'] = action.id
+        dct['datetime'] = action.datetime
+        dct['type'] = attr['type']
+        
+        if 'data' in attr.keys():
+            for key, val in attr['data'].items():
+                dct['data_path_' + key] = val
+        if 'info' in action.modules.keys():     
+            dct = {**dct, **dict(action.modules['info'])}
+        ls_actions.append(dct)
+    df = pd.DataFrame(ls_actions)
+    return df
